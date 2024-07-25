8f740f0f903db87c42799d8674e8a8bed610ab64
yhuangy <ythuangyt@gmail.com>
2024-07-25 12:56:12 +0800

    edit readme

--- wk12_dashboard_sg.py
+++ wk12_dashboard_sg.py
@@ -3,7 +3,7 @@ import numpy as np
 import geopandas as gpd
 from plotnine import *
 import streamlit as st
-import time
+#import time
 
 # Run in command line to run app
 # streamlit run wk12_dashboard_sg.py
